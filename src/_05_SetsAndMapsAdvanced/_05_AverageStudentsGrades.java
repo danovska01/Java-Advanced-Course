bf81c86dd765d9888bfea26481ab402da932d4f0
kristina <kristina.danovska@gmail.com>
2023-01-28 14:23:15 +0200

    #5

--- src/_05_SetsAndMapsAdvanced/_05_AverageStudentsGrades.java
+++ src/_05_SetsAndMapsAdvanced/_05_AverageStudentsGrades.java
@@ -4,35 +4,32 @@ import java.util.*;
 
 public class _05_AverageStudentsGrades {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        Map<String, List<Double>>namesWithGrades = new TreeMap<>();
-        for (int i = 0; i < n; i++) {
-            String [] tokens = scanner.nextLine().split("\\s+");
-            String name = tokens[0];
-            double grade= Double.parseDouble(tokens[1]);
-            if(!namesWithGrades.containsKey(name)){
-                List<Double>grades = new ArrayList<>();
-                grades.add(grade);
-                namesWithGrades.put(name, grades);
-            }
-            else {
-                List <Double>grades = namesWithGrades.get(name);
-                grades.add(grade);
-                namesWithGrades.put(name, grades);
-            }
+        Scanner scan = new Scanner(System.in);
+        Map<String, List<Double>> students = new TreeMap<>();
+        int counter = Integer.parseInt(scan.nextLine());
+        while (counter-- > 0) {
+            String[] tokens = scan.nextLine().split("\\s+");
+            String student = tokens[0];
+            double grade = Double.parseDouble(tokens[1]);
+
+            students.putIfAbsent(student, new ArrayList<>());
+            students.get(student).add(grade);
         }
 
-        for (String studentName : namesWithGrades.keySet()) {
-            System.out.print(studentName + " -> ");
-            double gradeSum = 0;
-            List<Double> studentGrades = namesWithGrades.get(studentName);
-            for (Double grade : studentGrades) {
-                gradeSum += grade;
-                System.out.printf("%.2f ", grade);
-            }
-            System.out.printf("(avg: %.2f)\n", Math.round(gradeSum / studentGrades.size() * 100) / 100.0);
-        }
+        students.entrySet()
+                .stream()
+                .forEach(entry -> {
+                    double sum = 0;
+                    for (int i = 0; i < entry.getValue().size(); i++) {
+                        sum += entry.getValue().get(i);
+                    }
+                    double avgSum = sum / entry.getValue().size();
+
+                    System.out.print(entry.getKey() + " -> ");
+                    entry.getValue().forEach(e -> System.out.printf("%.2f ", e));
+                    System.out.printf("(avg: %.2f)%n", avgSum);
+                });
+
 
         }
 }
