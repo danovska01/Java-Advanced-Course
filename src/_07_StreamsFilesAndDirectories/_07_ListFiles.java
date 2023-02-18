eb934677bad389b94746a41d4ffae753bffba079
kristina <kristina.danovska@gmail.com>
2023-02-18 13:24:59 +0200

    #7

--- src/_07_StreamsFilesAndDirectories/_07_ListFiles.java
+++ src/_07_StreamsFilesAndDirectories/_07_ListFiles.java
@@ -1,21 +1,19 @@
 package _07_StreamsFilesAndDirectories;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class _07_ListFiles {
     public static void main(String[] args) {
         //You are provided a folder named "Files-and-Streams". Create a program that lists the names and file sizes (in bytes)
         // of all files that are placed directly in it (do not include files in nested folders).
         //Use the File class and its method listFiles().
 
-        File file = new File("/home/kristina/IdeaProjects/Java-Advanced/src/_07_StreamsFilesAndDirectories/Files-and-Streams");
-        if(file.exists()){
-            if(file.isDirectory()){
-                File [] files = file.listFiles();
-                if (files != null) {
-                    for(File f: files){
+        File folder = new File("/home/kristina/IdeaProjects/Java-Advanced/src/_07_StreamsFilesAndDirectories/Files-and-Streams");
+
+        if(folder.exists()){
+            if(folder.isDirectory()){
+                File [] listOfFiles = folder.listFiles();
+                if (listOfFiles != null) {
+                    for(File f: listOfFiles){
                         if(!f.isDirectory()){
                             System.out.printf("%s: [%s]%n", f.getName(), f.length());
                         }
