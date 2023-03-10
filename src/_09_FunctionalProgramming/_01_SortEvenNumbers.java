1b0b62c0aaa80f2c5f4235394cc293bf91dd76cf
kristina <kristina.danovska@gmail.com>
2023-03-10 23:55:58 +0200

    #1

---
+++ src/_09_FunctionalProgramming/_01_SortEvenNumbers.java
@@ -0,0 +1,21 @@
+package _09_FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _01_SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] stringNumbers = scanner.nextLine().split(", ");
+        List<String>evenNumbers = Arrays.stream(stringNumbers)
+                .map(e ->Integer.parseInt(e))
+                .sorted()
+                .filter(e->e%2==0)
+                .map(e->e.toString())
+                .collect(Collectors.toList());
+
+        System.out.println(String.join(", ", evenNumbers));
+    }
+}
