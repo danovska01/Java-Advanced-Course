a6df53345c69d3365f43e4e8297283d1c1659e02
kristina <kristina.danovska@gmail.com>
2023-03-11 12:41:32 +0200

    #1

--- src/_09_FunctionalProgramming/_01_SortEvenNumbers.java
+++ src/_09_FunctionalProgramming/_01_SortEvenNumbers.java
@@ -9,6 +9,15 @@ public class _01_SortEvenNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String [] stringNumbers = scanner.nextLine().split(", ");
+
+        List<String>numbers = Arrays.stream(stringNumbers)
+                .map(e ->Integer.parseInt(e))
+                .filter(e->e%2==0)
+                .map(e->e.toString())
+                .collect(Collectors.toList());
+
+        System.out.println(String.join(", ", numbers));
+
         List<String>evenNumbers = Arrays.stream(stringNumbers)
                 .map(e ->Integer.parseInt(e))
                 .sorted()
