88da86893f1fbd52f4ab454413a7121cd9072f0c
kristina <kristina.danovska@gmail.com>
2023-04-12 16:29:23 +0300

    #1

---
+++ src/_17_IteratorsAndComparators/_1_Book/Book.java
@@ -0,0 +1,46 @@
+package _17_IteratorsAndComparators._1_Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        setTitle(title);
+        setYear(year);
+        setAuthors(authors); // превръщаме масива, който получаваме при точките горе директно в списък
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+
+
+}
