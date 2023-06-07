7d3af0aef1dc726c44f9301df95d0c9a46d308c4
kristina <kristina.danovska@gmail.com>
2023-06-07 11:40:21 +0300

    String to Character Demo

---
+++ src/_102_SNIPPETS/stringToChar.java
@@ -0,0 +1,10 @@
+package _102_SNIPPETS;
+
+public class stringToChar {
+    public static void main(String[] args) {
+        char variable = "c".charAt(0);
+        System.out.println(variable);
+
+
+    }
+}
