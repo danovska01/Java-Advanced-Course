05374e1432d45c3ef8c547b41a7266f32e6b15e0
kristina <kristina.danovska@gmail.com>
2023-06-07 11:42:53 +0300

    Character to int Demo

---
+++ src/_102_SNIPPETS/charToInt.java
@@ -0,0 +1,18 @@
+package _102_SNIPPETS;
+
+public class charToInt {
+    public static void main(String[] args) {
+        char variable1 = 'c'; // ascii number = 99
+        System.out.println((int)variable1);
+        System.out.println((char)99);
+
+
+
+        int int1= Integer.parseInt(String.valueOf('1'));
+        System.out.println(int1);
+
+
+        int int2= Character.getNumericValue('2');
+        System.out.println(int2);
+    }
+}
