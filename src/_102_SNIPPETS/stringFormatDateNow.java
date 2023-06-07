89c27810aa4f9e4e4cccf95a5aacfe189dbbab89
kristina <kristina.danovska@gmail.com>
2023-06-07 19:01:21 +0300

    date NOW string format

---
+++ src/_102_SNIPPETS/stringFormatDateNow.java
@@ -0,0 +1,7 @@
+package _102_SNIPPETS;
+
+public class stringFormatDateNow {
+    public static void main(String[] args) {
+        System.out.printf("%1$tH: %1$tM: %1$tS\n", new java.util.Date());
+    }
+}
