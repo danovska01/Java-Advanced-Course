c7e8dfec753992917ffa7bd53fabea52194dec32
kristina <kristina.danovska@gmail.com>
2023-06-07 11:46:21 +0300

    length of String

---
+++ src/_102_SNIPPETS/lengthOfString.java
@@ -0,0 +1,9 @@
+package _102_SNIPPETS;
+
+public class lengthOfString {
+    public static void main(String[] args) {
+        String text = "hey there";
+        int length = text.length();
+        System.out.println(length);
+    }
+}
