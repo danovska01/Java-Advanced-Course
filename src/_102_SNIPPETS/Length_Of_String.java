80d13598937e471e7adddb5d0d027ba47b9f617e
kristina <kristina.danovska@gmail.com>
2023-06-13 15:13:12 +0300

    rename to be more clear

--- src/_102_SNIPPETS/Length_Of_String.java
+++
@@ -1,9 +0,0 @@
-package _102_SNIPPETS;
-
-public class Length_Of_String {
-    public static void main(String[] args) {
-        String text = "hey there";
-        int length = text.length();
-        System.out.println(length);
-    }
-}
--- src/_102_SNIPPETS/Reverse_String.java
+++
@@ -1,9 +0,0 @@
-package _102_SNIPPETS;
-
-public class Reverse_String {
-    public static void main(String[] args) {
-        String variable = "Hey there";
-        String reversed = new StringBuilder(variable).reverse().toString();
-        System.out.println(reversed);
-    }
-}
